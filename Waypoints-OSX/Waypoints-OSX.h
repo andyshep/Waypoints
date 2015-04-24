42f8b9e5f804b204bf4f35f1f7bbfb7483fb0ee2
Andrew Shepard <shep.andy@gmail.com>
2015-04-24 12:38:21 -0700

    Add iOS target, rework build setup.

---
+++ Waypoints-OSX/Waypoints-OSX.h
@@ -0,0 +1,19 @@
+//
+//  Waypoints-OSX.h
+//  Waypoints-OSX
+//
+//  Created by Andrew Shepard on 4/24/15.
+//  Copyright (c) 2015 Andrew Shepard. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Waypoints-OSX.
+FOUNDATION_EXPORT double Waypoints_OSXVersionNumber;
+
+//! Project version string for Waypoints-OSX.
+FOUNDATION_EXPORT const unsigned char Waypoints_OSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Waypoints_OSX/PublicHeader.h>
+
+
---
+++ Waypoints-iOS/Waypoints.h
@@ -0,0 +1,19 @@
+//
+//  Waypoints-iOS.h
+//  Waypoints-iOS
+//
+//  Created by Andrew Shepard on 4/24/15.
+//  Copyright (c) 2015 Andrew Shepard. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Waypoints-iOS.
+FOUNDATION_EXPORT double Waypoints_iOSVersionNumber;
+
+//! Project version string for Waypoints-iOS.
+FOUNDATION_EXPORT const unsigned char Waypoints_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Waypoints_iOS/PublicHeader.h>
+
+
