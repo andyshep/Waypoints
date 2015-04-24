9204043f50832e2630949be328220a1133a39249
Andrew Shepard <shep.andy@gmail.com>
2015-04-24 15:20:56 -0700

    Correct framework version

--- Source/Waypoints.h
+++ Source/Waypoints.h
@@ -1,6 +1,6 @@
 //
-//  Waypoints-OSX.h
-//  Waypoints-OSX
+//  Waypoints.h
+//  Waypoints
 //
 //  Created by Andrew Shepard on 4/24/15.
 //  Copyright (c) 2015 Andrew Shepard. All rights reserved.
@@ -8,12 +8,12 @@
 
 #import <Foundation/Foundation.h>
 
-//! Project version number for Waypoints-OSX.
-FOUNDATION_EXPORT double Waypoints_OSXVersionNumber;
+//! Project version number for Waypoints.
+FOUNDATION_EXPORT double Waypoints_VersionNumber;
 
 //! Project version string for Waypoints-OSX.
-FOUNDATION_EXPORT const unsigned char Waypoints_OSXVersionString[];
+FOUNDATION_EXPORT const unsigned char Waypoints_VersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <Waypoints_OSX/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <Waypoints/PublicHeader.h>
 
 
